efbdde611930230a1c0a6519cdf40806096e19bb
Pavlo Marchuk <Pavlo Marchuk@DESKTOP-MQBICKJ>
2017-06-13 09:57:20 +0300

    1

--- 01_Continue/Program.cs
+++ 01_Continue/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _01_Continue
@@ -10,6 +11,52 @@ namespace _01_Continue
 	{
 		static void Main(string[] args)
 		{
+			Task task1 = new Task(MyTask);
+			//Task taskContinue = task1.ContinueWith(new Action<Task>(ContinueTask));
+			Task taskContinue = task1.ContinueWith(
+				tsk=>
+				{
+					Console.WriteLine("ContinueTask() запущен.");
+					for(int count = 0; count < 5; count++)
+					{
+						Thread.Sleep(500);
+						Console.WriteLine("В методе ContinueTask(), счетчик равен: " + count);
+					}
+					Console.WriteLine("ContinueTask() завершен.");
+				}
+				);
+			task1.Start();
+			taskContinue.Wait();
+			Console.WriteLine("Main Thread finished");
+			Console.ReadKey();
 		}
+		static void MyTask()
+		{
+			Console.WriteLine("MyTask() запущен.");
+
+			for(int count = 0; count < 5; count++)
+			{
+				Thread.Sleep(500);
+				Console.WriteLine("В методе MyTask(), счетчик равен: " + count);
+			}
+
+			Console.WriteLine("MyTask() завершен.");
+		}
+
+		static void ContinueTask(Task task)
+		{
+			Console.WriteLine("ContinueTask() запущен.");
+
+			for(int count = 0; count < 5; count++)
+			{
+				Thread.Sleep(500);
+				Console.WriteLine("В методе ContinueTask(), счетчик равен: " + count);
+			}
+
+			Console.WriteLine("ContinueTask() завершен.");
+		}
+
 	}
 }
+
+
---
+++ 02_EmployeeContitueWith/Program.cs
@@ -0,0 +1,39 @@
+﻿using _02_EmployeeContitueWith.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02_EmployeeContitueWith
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Main SATRT");
+			Task<IEnumerable<Employee>> task = new Task<IEnumerable<Employee>>(DataStorage.GetEmployee);
+			//task.Start();
+			//IEnumerable<Employee> collection  = DataStorage.GetEmployee();
+			task.ContinueWith(tsk => DataStorage.PrintEmployee(task.Result));
+			task.Start();
+			//DataStorage.PrintEmployee(task.Result);
+
+			Task taskDot = new Task(()=>
+			{
+				for(int i = 0; i < 120; i++)
+				{
+					Thread.Sleep(100);
+					Console.Write(".");
+				}
+			});
+			taskDot.Start();
+
+			Console.WriteLine("main END");
+			Console.ReadKey();
+			
+
+		}
+	}
+}
